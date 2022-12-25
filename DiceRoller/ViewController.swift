375cdbfeea6e9e26b6d1f27023f86ce01e877ee5
cagridoseyen <doseyenc@gmail.com>
2022-12-25 13:55:29 +0300

    dice roller done(first iOS project :D)

--- DiceRoller/ViewController.swift
+++ DiceRoller/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageOne: UIImageView!
+    
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    
+    private var diceNumberOne = 1
+    private var diceNumberTwo = 2
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        diceNumberOne = Int.random(in: 1...6)
+        diceNumberTwo = Int.random(in: 1...6)
+        
+        diceImageOne.image = UIImage(named:"\(diceNumberOne)")
+        diceImageTwo.image = UIImage(named:"\(diceNumberTwo)")
+    }
+    
 }
 
